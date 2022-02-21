9f1be709991a05c5c8158c9be71f96eed15e82d6
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-02-21 18:11:15 +0530

    add authorization

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jokes_App.Models;
+
+namespace Jokes_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Jokes_App.Models.Joke> Joke { get; set; }
+    }
+}
